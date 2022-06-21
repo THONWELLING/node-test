fdaee9f18134bf3a1374cbeb786545921a88cabd
THONWELLING <thonwellingdani13@gmail.com>
2022-06-20 23:21:19 -0300

    criando primeiro teste

---
+++ src/libs/Math.ts
@@ -0,0 +1,14 @@
+export const Math ={
+  sum: (n1: Number, n2: Number) => {
+
+  },
+  sub: (n1: Number, n2: Number) => {
+
+  },
+  div: (n1: Number, n2: Number) => {
+
+  },
+  mult: (n1: Number, n2: Number) => {
+
+  }
+}
